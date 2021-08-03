521a1a42984071518e3e1e961deef77045e5c459
tharindusenadeera <tharindusenadeera081@gmail.com>
2021-08-04 01:58:54 +0530

    pure node

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const url = new URL("https:facebook.com:8000/friends/dilshansajintha/friends?id=0011&NAME=KASUN&PROFILE=ACTIVE");
-url.searchParams.append('status','appended')
+const http = require('http');
 
-console.log(url.searchParams);
+const server = http.createServer( (req,res) => {
+   switch(req.url){
+       case "/stocks":
+           res.end("<h1>HELOOOOO</h1>");
+           break;
+        case "/aboutus":
+            res.end("0766324349");
+        case "/":
+            res.end("welcome to free pos");
+        default:
+            res.end();
+   }
+})
+
+server.listen(6000 , () => console.log("server running.."))
