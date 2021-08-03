b06bb9798527380ab638c856e8ac62aecc00343b
tharindusenadeera <tharindusenadeera081@gmail.com>
2021-08-03 21:17:13 +0530

    node recap

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const url = new URL("https:facebook.com:8000/friends/dilshansajintha");
+const url = new URL("https:facebook.com:8000/friends/dilshansajintha/friends?id=0011&NAME=KASUN&PROFILE=ACTIVE");
+url.searchParams.append('status','appended')
 
-console.log(url.search);
+console.log(url.searchParams);
